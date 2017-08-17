788ee9bf97c61a029a64b3e8808e3046de185b24
Javier Rebagliatti <javier.rebagliatti@southworks.com>
2017-08-16 23:37:25 -0300

    Added resized images

---
+++ images/_pngout.sh
@@ -0,0 +1,9 @@
+./pngout.exe 2017-05-12-Redback/vsm_initial.png
+./pngout.exe burgan/burgan_team.png
+./pngout.exe burgan/burgan_team1.png
+./pngout.exe burgan/custommenu.png
+./pngout.exe burgan/lookandfeel.png
+./pngout.exe burgan/notificationhub.png
+./pngout.exe burgan/notificationhub2.png
+./pngout.exe burgan/notificationhubPush.png
+
